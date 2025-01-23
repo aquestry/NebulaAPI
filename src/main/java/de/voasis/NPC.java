a5d8e3155fdc2f4f01368890fa3d924d2c37e095
aquestry <155507142+aquestry@users.noreply.github.com>
2025-01-23 18:25:28 +0100

    Update

--- src/main/java/de/voasis/NPC.java
+++ src/main/java/de/voasis/NPC.java
@@ -11,22 +11,22 @@ import java.util.Map;
 
 public class NPC extends Entity {
 
-    private final String nameNPC;
+    private final String NPCname;
 
     public NPC(String name, Instance instance, Pos spawn) {
         super(EntityType.PLAYER);
-        nameNPC = name;
+        NPCname = name;
         setBoundingBox(0, 0, 0);
         setNoGravity(true);
         setInstance(instance);
         scheduleNextTick(entity -> teleport(spawn));
-        NebulaAPI.createNametag(this, "<bold>"+nameNPC+"</bold>");
+        NebulaAPI.createNametag(this, NPCname);
     }
 
     @Override
     public void updateNewViewer(@NotNull Player player) {
         var properties = new ArrayList<PlayerInfoUpdatePacket.Property>();
-        var entry = new PlayerInfoUpdatePacket.Entry(getUuid(), nameNPC, properties, false, 0, GameMode.CREATIVE, null, null, 1);
+        var entry = new PlayerInfoUpdatePacket.Entry(getUuid(), NPCname, properties, false, 0, GameMode.CREATIVE, null, null, 1);
         player.sendPacket(new PlayerInfoUpdatePacket(PlayerInfoUpdatePacket.Action.ADD_PLAYER, entry));
         super.updateNewViewer(player);
         player.sendPackets(new EntityMetaDataPacket(getEntityId(), Map.of(17, Metadata.Byte((byte) 127))));
